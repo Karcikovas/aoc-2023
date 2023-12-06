a4313eeee35ebff98eb620ddc99b2a5006ae4492
Viktoras Karcikovas <viktoras.karcikovas@gmail.com>
2023-12-06 13:04:48 +0200

    hello.go

--- hello.go
+++ hello.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+	"regexp"
+	"strings"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	content, error := os.ReadFile("./assets/day1input.txt")
+	re := regexp.MustCompile(`[-]?\d[\d,]*[\.]?[\d{2}]*`)
+
+	if error != nil {
+		log.Fatal(error)
+	}
+
+	words := strings.Split(string(content), "\n")
+
+	for i := 0; i < len(words); i++ {
+		var str string = words[i]
+		values := re.FindAllString(str, -1)
+
+		if len(values) == 1 {
+			nmb1 := values[0]
+			fmt.Println(nmb1 + nmb1)
+		}
+	}
+
 }
