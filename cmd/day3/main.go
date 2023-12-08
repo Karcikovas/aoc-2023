f71f3d425e0a94199113bf2534a75aacd2c0ad1c
Viktoras Karcikovas <viktoras.karcikovas@gmail.com>
2023-12-08 12:57:22 +0200

    added day3 preparation

---
+++ cmd/day3/main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"aoc2023/internal/day3"
+)
+
+func main() {
+	day3.CalculatePart1()
+}
---
+++ internal/day3/day3.go
@@ -0,0 +1,9 @@
+package day3
+
+import (
+	"log"
+)
+
+func CalculatePart1() {
+	log.Println("Day 3 Solution:")
+}
