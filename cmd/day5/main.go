f371a93ac3199f209fdd10f0cd95f676238dd128
Viktoras Karcikovas <viktoras.karcikovas@gmail.com>
2023-12-14 12:20:51 +0200

    day 5 in progress

---
+++ cmd/day5/main.go
@@ -0,0 +1,12 @@
+package day5
+
+import (
+	"aoc2023/internal/day5"
+	"log"
+)
+
+func main() {
+	totalPointSum := day5.CalculatePart1()
+
+	log.Print("Day 5 result Part1 total scratch total cards value ", totalPointSum)
+}
---
+++ internal/day5/day5.go
@@ -0,0 +1,5 @@
+package day5
+
+func CalculatePart1() int {
+	return 0
+}
