146c0d257ea9e6b05290786174d59c7ac21b4598
karcikovas <viktoras.karcikovas@gmail.com>
2024-04-27 18:35:59 +0300

    Working on day 7

--- internal/day7/day.go
+++ internal/day7/day.go
@@ -2,82 +2,43 @@ package day7
 
 import (
 	"aoc2023/internal/utils"
-	"regexp"
+	"log"
 	"strings"
 )
 
 func CalculatePart1() int {
 	input := initialValues()
-	margin := 1
 
-	for _, race := range input.Races {
-		n := calculateNumberOfWinnings(race)
+	log.Println(input)
 
-		margin = margin * n
-	}
-
-	return margin
+	return 0
 }
 
-type Race struct {
-	RaceNumber int
-	Duration   int
-	Record     int
+type Hand struct {
+	Cards string
+	Bid   int
 }
 
-type Races struct {
-	Races []Race
-}
+func initialValues() []Hand {
+	var hands []Hand
 
-func initialValues() Races {
-	var times []string
-	var distances []string
-	races := Races{}
-
-	content := utils.ReadInput("./assets/day6input.txt")
+	content := utils.ReadInput("./assets/day7input.txt")
 	rows := strings.Split(string(content), "\n")
 
 	for _, row := range rows {
-		sliceIndex := strings.Index(row, ":")
-		valuesInString := row[sliceIndex+1:]
+		slice := strings.Split(row, " ")
+		cards := slice[0]
+		bid := slice[1]
 
-		fmtTime := "\\bTime\\b"
-		timePattern := regexp.MustCompile(fmtTime)
-		isTime := bool(timePattern.MatchString(row))
-		re := regexp.MustCompile(`\d+`)
+		if len(slice) >= 2 {
+			hand := Hand{
+				Cards: cards,
+				Bid:   utils.StringToInt(bid),
+			}
 
-		if isTime {
-			times = re.FindAllString(valuesInString, -1)
-		} else {
-			distances = re.FindAllString(valuesInString, -1)
+			hands = append(hands, hand)
 		}
 	}
 
-	for idx, time := range times {
-		race := Race{
-			RaceNumber: idx + 1,
-			Duration:   utils.StringToInt(time),
-			Record:     utils.StringToInt(distances[idx]),
-		}
-
-		races.Races = append(races.Races, race)
-	}
-
-	return races
-}
-
-func calculateNumberOfWinnings(race Race) int {
-	var numberOfWays int
-
-	for s := 0; s <= race.Duration; s++ {
-		timeLeft := race.Duration - s
-		distanceCovered := timeLeft * s
-
-		if distanceCovered > race.Record {
-
-			numberOfWays++
-		}
-	}
-
-	return numberOfWays
+	return hands
 }
