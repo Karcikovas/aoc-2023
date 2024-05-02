1e3806a2c2ed629aadca33516fc1f0950de3788f
karcikovas <viktoras.karcikovas@gmail.com>
2024-05-02 18:27:34 +0300

    Working on day 8

--- internal/day8/day.go
+++ internal/day8/day.go
@@ -2,149 +2,59 @@ package day8
 
 import (
 	"aoc2023/internal/utils"
-	"sort"
+	"log"
 	"strings"
 )
 
-const (
-	Ace   = 14
-	King  = 13
-	Queen = 12
-	Jack  = 11
-	Teen  = 10
-)
+type NodesMap = map[string]Node
+type NetworkInstructions = []string
 
-var cardValues = map[string]int{
-	"A": Ace,
-	"K": King,
-	"Q": Queen,
-	"J": Jack,
-	"T": Teen,
-	"9": 9,
-	"8": 8,
-	"7": 7,
-	"6": 6,
-	"5": 5,
-	"4": 4,
-	"3": 3,
-	"2": 2,
-}
-
-type HandWithStrength struct {
-	Cards    string
-	Bid      int
-	Strength int
-}
-
-type Hand struct {
-	Cards string
-	Bid   int
+type Node struct {
+	Key   string
+	Left  string
+	Right string
 }
 
 func CalculatePart1() int {
-	hands := initialValues()
-	handsWithStrength := getHandsWithStrength(hands)
-	handsOrderedByStrength := sortHands(handsWithStrength)
-	totalValue := calculateTotalWinnings(handsOrderedByStrength)
+	network, networkMap := initialValues()
 
-	return totalValue
+	log.Println(network, networkMap)
+
+	return 0
 }
 
-func calculateTotalWinnings(hands []HandWithStrength) int {
-	var totalValue int
+func initialValues() (NetworkInstructions, NodesMap) {
+	var network NetworkInstructions
+	var networkMap = make(NodesMap)
 
-	for i, hand := range hands {
-		totalValue = totalValue + (i+1)*hand.Bid
-	}
-
-	return totalValue
-}
-
-func sortHands(hands []HandWithStrength) []HandWithStrength {
-
-	sort.Slice(hands, func(i, j int) bool {
-		a := hands[i]
-		b := hands[j]
-
-		if a.Strength != b.Strength {
-			return a.Strength < b.Strength
-		}
-
-		for i := 0; i <= 5; i++ {
-			cardA := string(a.Cards[i])
-			cardB := string(b.Cards[i])
-			valueA := cardValues[cardA]
-			valueB := cardValues[cardB]
-
-			if valueA != valueB {
-				return valueA < valueB
-			}
-		}
-
-		return false
-	})
-
-	return hands
-}
-
-func getHandsWithStrength(hands []Hand) []HandWithStrength {
-	const maxAmountOfPairs = 2
-
-	var handsWithStrength []HandWithStrength
-	for _, hand := range hands {
-		counts := make(map[string]int)
-
-		for _, card := range hand.Cards {
-			c := string(card)
-
-			if utils.NumToBool(counts[c]) {
-				counts[c]++
-			} else {
-				counts[c] = 1
-			}
-		}
-
-		sortedItems := utils.SortMapByHighestValue(counts)
-
-		firstValue := sortedItems[0].Value
-		var secondValue = 1
-
-		if len(sortedItems) > 1 {
-			secondValue = sortedItems[1].Value
-		}
-
-		hand := HandWithStrength{
-			Cards:    hand.Cards,
-			Bid:      hand.Bid,
-			Strength: maxAmountOfPairs*firstValue + secondValue,
-		}
-
-		handsWithStrength = append(handsWithStrength, hand)
-	}
-
-	return handsWithStrength
-}
-
-func initialValues() []Hand {
-	var hands []Hand
-
-	content := utils.ReadInput("./assets/day7input.txt")
+	content := utils.ReadInput("./assets/day8input.txt")
 	rows := strings.Split(string(content), "\n")
 
-	for _, row := range rows {
-		slice := strings.Split(row, " ")
-		cards := slice[0]
-		bid := slice[1]
-
-		if len(slice) >= 2 {
-			hand := Hand{
-				Cards: cards,
-				Bid:   utils.StringToInt(bid),
+	for i, row := range rows {
+		if i == 0 {
+			for _, char := range row {
+				network = append(network, string(char))
 			}
+		}
 
-			hands = append(hands, hand)
+		if len(row) > 0 && i != 0 {
+			slices := strings.Split(row, " = ")
+			key := slices[0]
+			stringValues := slices[1]
+			log.Println(key, stringValues)
+
+			stringValues = strings.TrimLeft(stringValues, "(")
+			stringValues = strings.TrimRight(stringValues, ")")
+			values := strings.Split(stringValues, ", ")
+
+			node := Node{
+				Key:   key,
+				Left:  values[0],
+				Right: values[1],
+			}
+			networkMap[key] = node
 		}
 	}
 
-	return hands
+	return network, networkMap
 }
